8cd65b60b776a61f437336e7e6019fade6a14b26
Parth Sharma <parth261297@gmail.com>
2020-10-15 17:45:38 +0530

    add shapes.go

---
+++ structs-methods-and-interfaces/shapes_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "rectangle", shape: Rectangle{10, 10}, hasArea: 100.0},
+		{name: "circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+}
